00a4ce83ed5f6030cfb61e009f06de4a44ccc969
QualityTaster <akalnengn@gmail.com>
2023-09-28 14:13:15 +0300

    CompanyService Metods Updated

--- company-service/src/main/java/com/hrmanagement/controller/CompanyController.java
+++ company-service/src/main/java/com/hrmanagement/controller/CompanyController.java
@@ -7,6 +7,7 @@ import com.hrmanagement.dto.request.SaveCompanyRequestDto;
 import com.hrmanagement.repository.entity.Company;
 import com.hrmanagement.service.CompanyService;
 import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,41 +22,48 @@ import static com.hrmanagement.constants.EndPoints.*;
 public class CompanyController {
     private final CompanyService companyService;
 
-    @PostMapping("/save/{token}")
-    public ResponseEntity<Boolean> saveCompanyRequestDto(@PathVariable String token, @RequestBody SaveCompanyRequestDto dto){
-        return ResponseEntity.ok(companyService.save(token,dto));
+    @PostMapping("/save")
+    @Operation(summary = "Şirket kaydeder.")
+    public ResponseEntity<Boolean> saveCompanyRequestDto(@RequestBody SaveCompanyRequestDto dto){
+        return ResponseEntity.ok(companyService.save(dto));
     }
 
-    @GetMapping("/show-company-information/{token}")
-    public ResponseEntity<CompanyInformationResponseDto> showCompanyInformation(@PathVariable String token){
-        return ResponseEntity.ok(companyService.showCompanyInformation(token));
+    @GetMapping("/show-company-information")
+    @Operation(summary = "Id'si sorgulanan şirketin bilgilerini gösterir.")
+    public ResponseEntity<CompanyInformationResponseDto> showCompanyInformation(Long companyId){
+        return ResponseEntity.ok(companyService.showCompanyInformation(companyId));
     }
     @GetMapping("/find-all-company-preview-information")
+    @Operation(summary = "Bütün şirketlerin temel bilgilerini listeler.(Id,Logo,Şirket ismi,Posta Kodu,Şehir)")
     public ResponseEntity<List<VisitorCompanyInformations>> findAllCompanyPreviewInformation(){
         return ResponseEntity.ok(companyService.findAllCompanyPreviewInformation());
     }
 
     @GetMapping("/find-all-detailed-company-information/{companyId}")
+    @Operation(summary = "Id'si sorgulanan şirketin bütün bilgilerini getirir.")
     public ResponseEntity<VisitorDetailedCompanyInformationResponse> findCompanyDetailedInformation(@PathVariable Long companyId){
         return ResponseEntity.ok(companyService.findCompanyDetailedInformation(companyId));
     }
 
     @GetMapping("/find-all")
+    @Operation(summary = "Bütün şirketleri listeler.")
     public ResponseEntity<List<Company>> findAll(){
         return ResponseEntity.ok(companyService.findAll());
     }
 
 
     @GetMapping("/get-personnel-company-information/{companyId}")
+    @Operation(summary = "Id'si sorgulanan şirketin logosu ve şirketin ismiyle birlikte personel izin günlerini getirir.")
     public ResponseEntity<PersonnelCompanyInformationResponseDto> getPersonnelCompanyInformation(@PathVariable Long companyId){
         return ResponseEntity.ok(companyService.getPersonnelCompanyInformation(companyId));
     }
 
     @GetMapping("/does-company-exists/{companyId}")
+    @Operation(summary = "Id'si sorgulanan şirketin olup olmadığını kontrol eder.Geriye boolean döner.")
     public ResponseEntity<Boolean> doesCompanyExist(@PathVariable Long companyId){
         return ResponseEntity.ok(companyService.doesCompanyIdExist(companyId));
     }
-
+    @Hidden
     @GetMapping("/find-comments-with-company-name-by-status/{token}")
     public ResponseEntity<List<FindPendingCommentWithCompanyName>> findCommentWithCompanyNameByStatus(@PathVariable String token) {
         return ResponseEntity.ok(companyService.findCommentWithCompanyNameByStatus(token));
@@ -65,12 +73,12 @@ public class CompanyController {
     public ResponseEntity<String> getCompanyNameWithCompanyId(@PathVariable Long companyId) {
         return ResponseEntity.ok(companyService.getCompanyNameWithCompanyId(companyId));
     }
-
+    @Hidden
     @GetMapping("get-personnel-dashboard-information/{token}")
     public ResponseEntity<PersonnelDashboardResponseDto>getPersonnelDashboardInformation(@PathVariable String token){
         return ResponseEntity.ok(companyService.getPersonnelDashboardInformation(token));
     }
-
+    @Hidden
     @GetMapping("get-manager-dashboard-information/{token}")
     public ResponseEntity<ManagerDashboardResponseDto> getManagerDashboardInformation(@PathVariable String token){
         return ResponseEntity.ok(companyService.getManagerDashboardInformation(token));
@@ -96,7 +104,7 @@ public class CompanyController {
     public ResponseEntity<Boolean> doesCompanySubscriptionExist(@PathVariable Long companyId){
         return ResponseEntity.ok(companyService.doesCompanySubscriptionExist(companyId));
     }
-
+    @Hidden
     @PutMapping("/update-company-wage-date")
     ResponseEntity<Boolean> updateCompanyWageDate(@RequestBody UpdateCompanyWageDateResponseDto dto){
         return ResponseEntity.ok(companyService.updateCompanyWageDate(dto));
