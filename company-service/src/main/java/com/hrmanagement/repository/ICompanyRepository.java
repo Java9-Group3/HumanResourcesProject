68e26b598bab5c29220adc228b563af6e551cb49
efeaks88 <efe.aks@hotmail.com>
2023-10-02 19:37:28 +0300

    Frontend ile bağlantı kurabilmek için findbycompanyname eklenmesi.

--- company-service/src/main/java/com/hrmanagement/repository/ICompanyRepository.java
+++ company-service/src/main/java/com/hrmanagement/repository/ICompanyRepository.java
@@ -4,13 +4,11 @@ import com.hrmanagement.repository.entity.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface ICompanyRepository extends JpaRepository<Company,Long> {
     Boolean existsByCompanyNameIgnoreCase(String companyName);
     Boolean existsByCompanyId(Long companyId);
 
-    Optional<Company> findByCompanyName(String companyName);
+//    Optional<Company> findByCompanyName(String companyName);
 
 }
--- company-service/src/main/java/com/hrmanagement/service/CompanyService.java
+++ company-service/src/main/java/com/hrmanagement/service/CompanyService.java
@@ -297,12 +297,12 @@ public class CompanyService extends ServiceManager<Company, Long> {
         return true;
     }
 
-    public Company findCompanyByCompanyName(String companyName) {
-        Optional<Company> optionalCompany = companyRepository.findByCompanyName(companyName);
-        if (optionalCompany.isEmpty()) {
-            throw new CompanyManagerException(ErrorType.COMPANY_NOT_FOUND);
-        }
-        return optionalCompany.get();
-    }
+//    public Company findCompanyByCompanyName(String companyName) {
+//        Optional<Company> optionalCompany = companyRepository.findByCompanyName(companyName);
+//        if (optionalCompany.isEmpty()) {
+//            throw new CompanyManagerException(ErrorType.COMPANY_NOT_FOUND);
+//        }
+//        return optionalCompany.get();
+//    }
 
 }
